6552e58317120afd54afe2f69bc977b078b227bd
amird55 <amir.dgn@outlook.com>
2023-08-08 17:14:47 +0300

    גרסה ראשונית עם טופס הוספה חיצוני

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const port = 5252;
+const app = express();
+app.use(express.json());
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
+const path = require('path');
+//------------------------------------------------
+let AllData=[];
+
+//------------------------------------------------
+app.get("/",(req, res) => {
+    res.send("hello");
+});
+app.get("/List",(req, res) => {
+    res.send(AllData).json();
+});
+app.post("/Add",(req, res) => {
+    let line={};
+    line.name = req.body.name;
+    line.phone = req.body.phone;
+    line.pobox = req.body.pob;
+    AllData.push(line);
+    res.send("Ready to Add EndPoint");
+});
+
+//------------------------------------------------
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Now listening on port ${port}`);
+});
